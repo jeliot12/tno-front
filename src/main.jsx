847bde23507be7ab268c6e55f8135db44f8d93a3
qween <124870745+jeliot12@users.noreply.github.com>
2025-04-04 00:57:09 +0800

    hm3

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-// import { BrowserRouter } from "react-router-dom"
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom"
 // import UserStore from './store/UserStore.js'
 
 // export const Context = createContext(null)
-const isMobileDevice = () => {
-  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || 
-         (window.innerWidth <= 768 && window.innerHeight <= 1024) ||
-         ('ontouchstart' in window);
-};
-if (!isMobileDevice() && !window.location.pathname.includes('/desktop')) {
-  window.location.href = '/desktop';
-}else {createRoot(document.getElementById('root')).render(
-      <Router>
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <BrowserRouter>
           <App />
-      </Router>
-)}
+      </BrowserRouter>
+    </StrictMode>
+)
